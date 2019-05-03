fb4181d253fa58c9aa83c85bf13e7c512cc92808
OmarAlmo <omar.almoallim@gmail.com>
2019-05-03 19:49:04 -0400

    Update landing page with more data

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
 
-    def index
-      @users = User.all
-    end
-  
-    def new
-      @user = User.new
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(
-        :first_name, :last_name, :country, 
-        :city, :email, :password, 
-        :password_confirmation, :remember_me)
-    end
-  
-  
+  def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name, :last_name, :country_id, 
+      :city_id, :email, :password, 
+      :password_confirmation, :remember_me)
+  end
+
+
+end
